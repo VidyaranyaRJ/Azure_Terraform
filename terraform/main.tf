23bb28b3f600e50d5d3a38a91e7df9f235bbe61a
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-03-29 05:15:27 -0600

    a31

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,34 @@ terraform {
 
 provider "azurerm" {
   features {}
+  
+  # Add these lines for service principal authentication
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
+# Add these variables
+variable "client_id" {
+  description = "Azure Service Principal client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
 }
 
 variable "prefix" {
